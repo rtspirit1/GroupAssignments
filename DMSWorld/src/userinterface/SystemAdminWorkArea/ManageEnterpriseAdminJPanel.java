d42b6e93c46d720e41fd9f4efe93c2d6176ae644
rtspirit1 <tiwari.ra@northeastern.edu>
2021-04-21 17:12:33 -0400

    New changes

--- DMSWorld/src/userinterface/SystemAdminWorkArea/ManageEnterpriseAdminJPanel.java
+++ DMSWorld/src/userinterface/SystemAdminWorkArea/ManageEnterpriseAdminJPanel.java
@@ -111,35 +111,9 @@ public class ManageEnterpriseAdminJPanel extends javax.swing.JPanel {
         setPreferredSize(new java.awt.Dimension(1058, 840));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
-
-        jLabel9.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel9.setFont(new java.awt.Font("Cambria", 1, 24)); // NOI18N
-        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel9.setText("DMS World");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(441, 441, 441)
-                .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(484, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(21, 21, 21))
-        );
-
-        add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1060, 70));
-
         jLabel1.setFont(new java.awt.Font("Cambria", 1, 18)); // NOI18N
         jLabel1.setText("Enterprise Management Admin Screen");
-        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, 340, 30));
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 20, 340, 30));
 
         jLabel2.setFont(new java.awt.Font("Cambria", 1, 14)); // NOI18N
         jLabel2.setText("Network:");
@@ -239,6 +213,15 @@ public class ManageEnterpriseAdminJPanel extends javax.swing.JPanel {
 
     }//GEN-LAST:event_networkJComboBoxActionPerformed
 
+    private void enterpriseJComboBoxActionPerformed(java.awt.event.ActionEvent evt) {                                                    
+
+//        Network network = (Network) enterpriseJComboBox.getSelectedItem();
+//        if (network != null){
+//            populateEnterpriseComboBox(network);
+//        }
+
+    } 
+    
     private void SubmitjButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SubmitjButtonActionPerformed
         // TODO add your handling code here:
         Enterprise enterprise = (Enterprise) enterpriseJComboBox.getSelectedItem();
